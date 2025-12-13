095419c8dab0c5a5c636ba2b10f71e717e5fce42
Pankhil29 <pankhilpatel734@gmail.com>
2025-12-13 22:34:58 +0530

    post by category

--- blogs/views.py
+++ blogs/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse 
+from .models import Blog
 
-# Create your views here.
+def posts_by_category(req,category_id):
+    posts = Blog.objects.filter(status="Published",category=category_id)
+    # context = {
+    #     "posts":posts,
+    # }
+    return render(req,'posts_by_category.html',posts)
