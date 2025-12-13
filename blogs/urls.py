2aae61a8f117becf6f66aac1c74ff97b0e7ef1f8
Pankhil29 <pankhilpatel734@gmail.com>
2025-12-13 22:39:54 +0530

    solve the url

--- blogs/urls.py
+++ blogs/urls.py
@@ -5,5 +5,5 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('<int:pk_id>',views.posts_by_category,name="posts_by_category"),
+    path('<int:category_id>',views.posts_by_category,name="posts_by_category"),
 ] 
