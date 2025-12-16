753c85940abb97979c3387172e60cf75c1fe0e48
Pankhil29 <pankhilpatel734@gmail.com>
2025-12-16 17:21:05 +0530

    categpry by posts

--- blog_main/views.py
+++ blog_main/views.py
@@ -2,14 +2,14 @@ from django.shortcuts import render
 from blogs.models import Category,Blog
 
 def home(req):
-    categories = Category.objects.all()
+    # categories = Category.objects.all()
     featured_posts = Blog.objects.filter(is_featured=True,status='Published').order_by('updated_at')
     posts = Blog.objects.filter(is_featured=False,status='Published')
     print(posts)
     # print(featured_posts)
     # print(categories)
     context = {
-        'categories': categories,
+        # 'categories': categories,
         'featured_posts' : featured_posts,
         'posts' : posts,
     }
