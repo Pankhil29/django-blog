144089cc929c0e4df40db8b0c97c6cd7797ad62f
Pankhil29 <pankhilpatel734@gmail.com>
2026-01-01 10:21:28 +0530

    error slove

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -11,7 +11,9 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
+SECRET_KEY = os.environ.get("DJANGO_SECRET_KEY")
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
